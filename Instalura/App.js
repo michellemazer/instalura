5c5ebfdcb5d97cccb9975bf1be69189826edd35a
Flavio Barbosa <flavio@ciaware.com.br>
2021-02-05 18:52:01 -0300

    Aula2

--- Instalura/App.js
+++ Instalura/App.js
@@ -7,14 +7,46 @@
  */
 
 import React, {Fragment} from 'react';
-import {Text} from 'react-native';
+import {
+  Text, 
+  Image, 
+  ScrollView,
+  Dimensions,
+  StyleSheet,
+  FlatList,
+} from 'react-native';
+
+const largura = Dimensions.get("screen").width;
+const informacoes = [
+  { usuario: "Michelle" }, 
+  { usuario: "Flavio" },
+  { usuario: "Ricardo" },
+  { usuario: "Gabriel" },
+  { usuario: "Leonardo" }
+];
 
 const App = () => {
   return (
-    <Fragment>
-      <Text>Hello World!</Text>
-    </Fragment>
+    <ScrollView>
+      <FlatList 
+        data={informacoes}
+        renderItem={({item}) => 
+          <Fragment>
+            <Text>{item.usuario}</Text>
+            <Image source={require("./res/img/alura.jpg")} style={estilo.imagem}/>
+          </Fragment>
+        }
+      />
+      
+    </ScrollView>
   );
 };
+
+const estilo=StyleSheet.create({
+  imagem:{
+    width:largura,
+    height:largura
+  }
+});
 
 export default App;
